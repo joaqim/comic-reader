035c465f6bb50e3f21af446be5118a3011e7d847
Joaqim Planstedt <joaqimp@gmail.com>
2019-08-10 12:48:04 +0200

    Updates

---
+++ precache-manifest.8d3807359bb72c11e6eb45fb50bd49e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e63e7fa27d21f53501a22b353c00eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c73f9c317279b7f4ee8",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a8989588cb053d9e3d28",
+    "url": "/static/js/2.63d0756c.chunk.js"
+  },
+  {
+    "revision": "8c73f9c317279b7f4ee8",
+    "url": "/static/js/main.1d8b2cd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
